e8f4b25f28103959535ce255c95437f6ce461886
Ole-Johan Øvreås <olejohanovreas@gmail.com>
2024-08-24 10:49:56 +0200

    Clear the screen on exit

--- kilo.c
+++ kilo.c
@@ -18,6 +18,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -63,6 +66,8 @@ void editorProcessKeypresses() {
 
     switch (c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
